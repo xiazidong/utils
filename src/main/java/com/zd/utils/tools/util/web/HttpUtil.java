c4c8a84de166d8c658ae2afea3808a82a6c617ec
夏紫东 <zidong.me@gmail.com>
2019-04-19 16:56:34 +0800

    优化 url获取方式

--- src/main/java/com/zd/utils/tools/util/web/HttpUtil.java
+++ src/main/java/com/zd/utils/tools/util/web/HttpUtil.java
@@ -21,6 +21,10 @@ import java.net.URI;
 import java.net.URL;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static com.zd.utils.constant.HTTPMethod.GET_REQUEST;
 
@@ -53,7 +57,8 @@ public class HttpUtil {
     }
 
     /**
-     * 获取当前服务的 URL
+     * 获取当前服务的 URL，不包含参数
+     * ex: http://127.0.0.1:10889
      * @return url
      */
     public static String getSelfUrl() {
@@ -115,5 +120,45 @@ public class HttpUtil {
         OutputStream out = response.getOutputStream();
         out.write(userJson.getBytes(StandardCharsets.UTF_8));
         out.flush();
+    }
+
+    /**
+     * 获取完整 url，带参数
+     */
+    public static String getFullUrl(HttpServletRequest request) {
+        String url = request.getRequestURL().toString();
+        String queryurl = request.getQueryString();
+        if (null != queryurl) {
+            url += "?" + queryurl;
+        }
+        return url;
+    }
+
+    /**
+     * 获取 url中的参数
+     * @param url
+     * @return Map集合
+     */
+    public static Map<String, List<String>> getQueryParams(String url) {
+        try {
+            Map<String, List<String>> params = new HashMap<>();
+            String[] urlParts = url.split("\\?");
+            if (urlParts.length > 1) {
+                String query = urlParts[1];
+                for (String param : query.split("&")) {
+                    String[] pair = param.split("=");
+                    String key = URLDecoder.decode(pair[0], "UTF-8");
+                    String value = "";
+                    if (pair.length > 1) {
+                        value = URLDecoder.decode(pair[1], "UTF-8");
+                    }
+                    List<String> values = params.computeIfAbsent(key, k -> new ArrayList<>());
+                    values.add(value);
+                }
+            }
+            return params;
+        } catch (UnsupportedEncodingException ex) {
+            throw new AssertionError(ex);
+        }
     }
 }
