52d1555c7b30933fb5d99626e0754db310da79f8
夏紫东 <zidong.me@gmail.com>
2019-05-08 19:53:47 +0800

    增加脱敏处理类

---
+++ src/main/java/com/zd/utils/constant/SensitiveTypeEnum.java
@@ -0,0 +1,41 @@
+package com.zd.utils.constant;
+
+/**
+ * @author Zidong
+ * @date 2019/5/8 7:48 PM
+ */
+public enum SensitiveTypeEnum {
+    /**
+     * 中文名
+     */
+    CHINESE_NAME,
+    /**
+     * 身份证号
+     */
+    ID_CARD,
+    /**
+     * 座机号
+     */
+    FIXED_PHONE,
+    /**
+     * 手机号
+     */
+    MOBILE_PHONE,
+    /**
+     * 地址
+     */
+    ADDRESS,
+    /**
+     * 电子邮件
+     */
+    EMAIL,
+    /**
+     * 银行卡
+     */
+    BANK_CARD,
+    /**
+     * 密码
+     */
+    PASSWORD;
+
+}
